11a2e58c2bb8bc421a4bdcfcab2a4940305af00a
LucaArcudi <arcudilu@gmail.com>
2022-11-25 07:07:06 +0100

    implemented email checker

--- JS/script.js
+++ JS/script.js
@@ -7,24 +7,49 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 
+
+
+// DICE GAME
+
+// let button = document.querySelector("a.btn");
+// button.addEventListener("click", function (){
+//     let userPickedNumber = Math.floor(Math.random() * 6 + 1);
+//     document.querySelector("pre.user-number").innerHTML = `Il tuo numero: ${userPickedNumber}`;
+
+//     let pcPickedNumber = Math.floor(Math.random() * 6 + 1);
+//     document.querySelector("pre.pc-number").innerHTML = `Il tuo numero: ${userPickedNumber}`;
+
+//     if (userPickedNumber > pcPickedNumber){
+//         document.getElementById("output").innerHTML = "Risultato: hai vinto!";
+//     }       else if (userPickedNumber == pcPickedNumber){
+//         document.getElementById("output").innerHTML = "Risultato: pareggio!";
+//     }       else {
+//         document.getElementById("output").innerHTML = "Risultato: hai perso!";
+//     }
+// });
+
+
+
+// EMAIL CHECKER
+
+let userEmailElement = document.getElementById("email");
+let emails = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com"];
+
 let button = document.querySelector("a.btn");
 button.addEventListener("click", function (){
-    let userPickedNumber = Math.floor(Math.random() * 6 + 1);
-    document.querySelector("pre.user-number").innerHTML = `Il tuo numero: ${userPickedNumber}`;
+    let userEmailValue = userEmailElement.value;
 
-    let pcPickedNumber = Math.floor(Math.random() * 6 + 1);
-    document.querySelector("pre.pc-number").innerHTML = `Il tuo numero: ${userPickedNumber}`;
+    let check = -1;
 
-    if (userPickedNumber > pcPickedNumber){
-        document.getElementById("output").innerHTML = "Risultato: hai vinto!";
-    }       else if (userPickedNumber == pcPickedNumber){
-        document.getElementById("output").innerHTML = "Risultato: pareggio!";
-    }       else {
-        document.getElementById("output").innerHTML = "Risultato: hai perso!";
+    for (let i = 0; i < emails.length; i++){
+        if (userEmailValue == emails[i]){
+            check = i
+        } 
     }
+
+    if (check >= 0) {
+    document.getElementById("output").innerHTML = `The email: ${userEmailValue} is already existing.`;
+}   else {
+    document.getElementById("output").innerHTML = `The email: ${userEmailValue} is available.`;
+}
 });
-
-
-
-
-
