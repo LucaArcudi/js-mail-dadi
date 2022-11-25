f4e5dd012260153a5c733120642279454980f443
LucaArcudi <arcudilu@gmail.com>
2022-11-25 04:55:13 +0100

    implemented dice game

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+let button = document.querySelector("a.btn");
+button.addEventListener("click", function (){
+    let userPickedNumber = Math.floor(Math.random() * 6 + 1);
+    document.querySelector("pre.user-number").innerHTML += userPickedNumber;
+
+    let pcPickedNumber = Math.floor(Math.random() * 6 + 1);
+    document.querySelector("pre.pc-number").innerHTML += pcPickedNumber;
+
+    if (userPickedNumber > pcPickedNumber){
+        document.getElementById("output").innerHTML = "Hai vinto";
+    }       else if (userPickedNumber == pcPickedNumber){
+        document.getElementById("output").innerHTML = "Pareggio";
+    }       else {
+        document.getElementById("output").innerHTML = "Hai perso";
+    }
+});
+
+
+
+
+
